d637d17b26c3d080bff65290f3328ca64523b116
lucasgmpaiva <lucasgmpaiva1@gmail.com>
2021-03-22 12:25:40 -0300

    Adicionando Teste Para GetMovie

---
+++ APIProjectMoviesWebII/src/test/java/com/project/movies/MovieTest.java
@@ -0,0 +1,35 @@
+package com.project.movies;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import com.project.movies.entity.MovieEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class MovieTest {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Test
+	public void testGetMovie() {
+		
+		ResponseEntity<MovieEntity> res = testRestTemplate.getForEntity("http://localhost:" + port + "/movie/1", MovieEntity.class);
+		
+		MovieEntity movie = res.getBody();
+		
+		assertEquals(1L, movie.getId());
+		
+	}
+
+}
