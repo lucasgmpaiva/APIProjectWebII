726b4562d049710f10bd97d2263b5df2408529f0
lucas.paiva <lucas.paiva@esig.com.br>
2021-04-18 00:57:13 -0300

    ADicionando criação de tabela

--- APIProjectMoviesWebII/Scripts/createDB.sql
+++ APIProjectMoviesWebII/Scripts/createDB.sql
@@ -1,1 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS moviesDB;
+
+use moviesDB;
+
+create table movie_entity (id bigint not null auto_increment, director varchar(140) not null, gender varchar(140) not null, release_date varchar(140) not null, title varchar(140) not null, writer varchar(140) not null, primary key (id));
+
+create table serie_entity (id bigint not null auto_increment, broadcaster varchar(140) not null, gender varchar(140) not null, release_date varchar(140) not null, title varchar(140) not null, primary key (id));
