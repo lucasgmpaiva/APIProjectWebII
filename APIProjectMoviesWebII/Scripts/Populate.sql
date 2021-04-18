e2f9fc7c735099f782c306dae4e170a4a853980d
lucas.paiva <lucas.paiva@esig.com.br>
2021-04-18 10:29:27 -0300

    Adicionando script de popular séries

--- APIProjectMoviesWebII/Scripts/Populate.sql
+++ APIProjectMoviesWebII/Scripts/Populate.sql
@@ -5,3 +5,9 @@ INSERT INTO movie_entity (director, gender, release_date, title, writer) VALUES 
 INSERT INTO movie_entity (director, gender, release_date, title, writer) VALUES ('Spielberg', 'Family', 'Today', 'Test 2 - A Revanche', 'Spielberg');
 
 INSERT INTO movie_entity (director, gender, release_date, title, writer) VALUES ('Spielberg', 'Family', 'Today', 'Test 3 - De Novo?', 'Spielberg');
+
+INSERT INTO serie_entity (gender, release_date, title, broadcaster) VALUES ('Novela', 'Há muito tempo', 'Laços de Família', 'Globo');
+
+INSERT INTO serie_entity (gender, release_date, title, broadcaster) VALUES ('Novela', '2012', 'Avenida Brasil', 'Globo');
+
+INSERT INTO serie_entity (gender, release_date, title, broadcaster) VALUES ('Novela', 'Há Mais Tempo Ainda', 'O Rei do Gado', 'Globo');
